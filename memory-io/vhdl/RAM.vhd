974b404d3075f7dc7885d8234a9d478efe2632e1
Paul Spillane <spillpa@gmail.com>
2011-11-08 00:43:56 -0600

    Added documentation, VHDL files, and memory list files.

---
+++ memory-io/vhdl/RAM.vhd
@@ -0,0 +1,83 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:26:53 11/07/2011 
+-- Design Name: 
+-- Module Name:    RAM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: DATA MEMORY
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_TEXTIO.ALL;
+use STD.TEXTIO.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RAM is
+port ( 	clock	: 	in std_logic; 	
+	memWrite	: 	in std_logic;
+	memAddress : 	in std_logic_vector(31 downto 0); --memory address correct size?
+	writeData	: 	in std_logic_vector(31 downto 0);
+	readData : out std_logic_vector(31 downto 0)
+);
+end RAM;
+
+architecture Behavioral of RAM is
+
+  type RamType is array (0 to 31) of --words
+        std_logic_vector(31 downto 0); --bits
+  
+  -- Subprogram to read a text file into RAM     
+	impure function Load_RAM (RamFileName: in string) return RamType is
+        -- Open File in Read Mode
+         FILE ramfile   :text is in RamFileName;
+         variable RamFileLine  :line;
+         variable RAM : RamType;
+     begin
+				for I in RamType'range loop
+						-- read digital data from input file
+						 readline(ramfile, RamFileLine);
+						 exit when endfile (ramfile);
+						 read(RamFileLine, RAM(I));
+				end loop;
+		return RAM;
+     end function;
+	  
+	  signal temp: RamType := Load_RAM("memory.list");
+
+begin
+
+  writeMem: process(clock, memWrite, writeData)
+  begin
+		if (clock'event and clock = '0') then
+		  if memWrite='1' then
+			 temp(conv_integer(memAddress)) <= writeData;
+		  end if;
+		end if;
+  end process;						   
+	
+  readMem: process(clock, memAddress)
+  begin	
+		readData <= temp(conv_integer(memAddress));
+  end process;
+
+
+
+end Behavioral;
+
---
+++ memory-io/vhdl/ROM.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:31:42 11/07/2011 
+-- Design Name: 
+-- Module Name:    ROM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: INSTRUCTION MEMORY
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_TEXTIO.ALL;
+use STD.TEXTIO.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ROM is
+port (
+	addressIn : 	in std_logic_vector(31 downto 0);  
+	instrucOut : out std_logic_vector(31 downto 0)
+);
+end ROM;
+
+architecture Behavioral of ROM is
+
+  type MemType is array (0 to 31) of --words
+        std_logic_vector(31 downto 0); --bits
+  
+  -- Subprogram to read a text file into RAM     
+	impure function Load_Mem (MemFileName: in string) return MemType is
+        -- Open File in Read Mode
+         FILE MemFile   :text is in MemFileName;
+         variable MemFileLine  :line;
+         variable InstrMem : MemType;
+     begin
+         for I in MemType'range loop
+            -- read digital data from input file
+             readline(MemFile, MemFileLine);
+				 exit when endfile (MemFile);
+             read(MemFileLine, InstrMem(I));
+         end loop;
+		return InstrMem;
+     end function;
+	  
+	  signal temp: MemType := Load_Mem("instr.list");
+
+begin	   
+	
+  readMem: process(addressIn)
+  begin
+		instrucOut <= temp(conv_integer(addressIn));
+  end process;
+
+end Behavioral;
+
